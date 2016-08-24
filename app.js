082db8012d5598764a453ac516c7b572ba87b23c
eveness <eveness@gmail.com>
2016-08-25 01:44:51 +0300

    Глава 1-4

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var fortune = require('./lib/fortune.js');
+
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('home', { fortune: fortune.getFortune() });
+});
+
+app.use(function(req, res, next) {
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next) {
+	console.log(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+	console.log('Express запущен на http://localhost:' + app.get('port'));
+});
