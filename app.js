e13f62564b24b8aace6c7fc81c7a20fe6f32c8de
eveness <eveness@gmail.com>
2016-08-29 14:09:32 +0300

    Глава 8, обработка форм посредством AJAX

--- app.js
+++ app.js
@@ -74,11 +74,13 @@ app.get('/newsletter', function(req, res) {
 	res.render('newsletter', { csrf: 'CSRF token' });
 });
 app.post('/process', function(req, res) {
-	console.log('Form (from querystring): ' + req.query.form);
-	console.log('CSRF token (from hidden form field): ' + req.body._csrf);
-	console.log('Name (from visible form field): ' + req.body.name);
-	console.log('Email (from visible form field): ' + req.body.email);
-	res.redirect(303, '/thank-you');
+	if(req.xhr || req.accepts('json,html') === 'json') {
+		// при ошибке отправлять { error: 'описание ошибки' }
+		res.send({ success: true });
+	} else {
+		// при ошибке перенаправлять на страницу ошибки
+		res.redirect(303, '/thank-you');
+	}
 });
 app.get('/thank-you', function(req, res) {
 	res.render('thank-you');
