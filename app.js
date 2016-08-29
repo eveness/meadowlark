b29c85d7a07886c00a29d3e3f1b106420c69f0d9
eveness <eveness@gmail.com>
2016-08-29 14:31:47 +0300

    Глава 8, обработка форм, загрузка файлов на сервер

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var formidable = require('formidable');
 var fortune = require('./lib/fortune.js');
 var weather = require('./lib/weather.js');
 
@@ -84,6 +85,25 @@ app.post('/process', function(req, res) {
 });
 app.get('/thank-you', function(req, res) {
 	res.render('thank-you');
+});
+
+app.get('/contest/vacation-photo', function(req, res) {
+	var now = new Date();
+	res.render('contest/vacation-photo', {
+		year: now.getFullYear(),
+		month: now.getMonth()
+	});
+});
+app.post('/contest/vacation-photo/:year/:month', function(req, res) {
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, files) {
+		if(err) return res.redirect(303, '/error');
+		console.log('recieved fields:');
+		console.log(fields);
+		console.log('recieved files:');
+		console.log(files);
+		res.redirect(303, '/thank-you');
+	});
 });
 
 app.get('/headers', function(req, res) {
